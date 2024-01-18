5af5ca2a36d07a3b3417bb8282d961f7140b9187
alexprp <alexprphillips@gmail.com>
2024-01-18 13:05:13 -0500

    better trc script

--- 2024_tetrahymena_temp_response_curve.R
+++ 2024_tetrahymena_temp_response_curve.R
@@ -1,9 +1,21 @@
-##Install Packages
+# Tell R which program to use: ggplot2, a plotting powerhouse
 library(ggplot2)
-tpcs <- read.csv("./Tetra_TPC_24h.csv")
-ggplot(tpcs, aes(x = Temperature, y = R)) +
+
+# Tell R to read in the data file
+tpcs = read.csv("./Tetra_TPC_24h.csv")
+
+# Plot with ggplot2
+ggplot(data = tpcs, aes(x = Temperature, y = R)) +
   geom_point() +
   theme_bw(base_size = 12) +
   labs(x = 'Temperature (ºC)',
-       y = 'R',
-       title = '24 h Growth across temperatures')
+       y = 'R = ln(final / initial)/days',
+       title = '24 h growth of Tetrahymena across temperatures')
+
+# Plot replicates in different colors
+ggplot(data = tpcs, aes(x = Temperature, y = R, color = Replicate)) +
+  geom_point() +
+  theme_bw(base_size = 12) +
+  labs(x = 'Temperature (ºC)',
+       y = 'R = ln(final / initial)/days',
+       title = '24 h growth of Tetrahymena across temperatures')
