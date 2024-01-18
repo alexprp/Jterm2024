34e1a2923e8170a4602d69cb0957ab26df14eb54
alexprp <alexprphillips@gmail.com>
2024-01-17 20:06:14 -0500

    tetra growth curve

--- 2024_tetrahymena_temp_response_curve.R
+++ 2024_tetrahymena_temp_response_curve.R
@@ -1,17 +1,9 @@
 ##Install Packages
-library(tidyverse)
 library(ggplot2)
-library(nls.multstart)##Prepare and insert file here
 tpcs <- read.csv("./Tetra_TPC_24h.csv")
-tpcs <- tpcs %>%
-  filter(Count>=1) %>%
-  mutate(r = log(Count/Initial)) %>%
-  mutate(r_scale=10,
-         log_r=log(r+r_scale)) %>%
-  ungroup##Display data
-ggplot(tpcs, aes(Temp, log_r)) +
+ggplot(tpcs, aes(x = Temperature, y = R)) +
   geom_point() +
   theme_bw(base_size = 12) +
   labs(x = 'Temperature (ºC)',
-       y = 'Log R',
+       y = 'R',
        title = '24 h Growth across temperatures')
